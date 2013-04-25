63e2e555e9d0b78002bd934b4b2435ae750c54f8
Ooms Aurélien <genius@gmail.com>
2013-04-25 12:24:14 +0200

    premier tp

---
+++ TP/h/ClausesGenerator.h
@@ -0,0 +1,18 @@
+#ifndef _CLAUSESGENERATOR_H
+#define _CLAUSESGENERATOR_H
+
+#include <vector>
+#include <cstring>
+
+
+class ClausesGenerator{
+	private:
+		void generateClausesRecursively(std::vector<std::vector<int>>& clauses, std::vector<int> neighbours, int mines, size_t offset);
+		std::vector<std::vector<int>> generateClauses(std::vector<int> neighbours, int mines);
+		int f(int n);
+	public:
+		std::vector<std::vector<int>> run(std::vector<std::vector<char>> table);
+};
+
+
+#endif // _CLAUSESGENERATOR_H
